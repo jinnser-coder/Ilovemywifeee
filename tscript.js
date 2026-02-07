0714e33cbb3558d47d8198115b41b0b5457ef538
Jithin <144246229+jinnser-coder@users.noreply.github.com>
2026-02-08 00:29:43 +0530

    Update tscript.js

--- tscript.js
+++ tscript.js
@@ -43,4 +43,4 @@ nextBtn.addEventListener("click", () => {
       "I don't love you just as a time pass. I want to grow old with you, build a life with you, and someday marry you. No matter what problems come in life,enik ninne cherth pidich kude nirthanm, nee ellathe njan ella vavee :)";
     nextBtn.style.display = "none";
   }
-});</script>
+});
