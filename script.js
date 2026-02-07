5978e0409847eb3c2ad23b0ed484c2d21a3c2804
Jithin <144246229+jinnser-coder@users.noreply.github.com>
2026-02-07 23:51:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+<meta name='viewport' content='width=device-width, initial-scale=1'/><script>      nextBtn.textContent = "💌 ";
+    }
+  } else {
+    title.textContent = "And....";
+    message.textContent =
+      "I don't love you just as a time pass. I want to grow old with you, build a life with you, and someday marry you. No matter what problems come in life,enik ninne cherth pidich kude nirthanm, nee ellathe njan ella vavee :)";
+    nextBtn.style.display = "none";
+  }
+});</script>
